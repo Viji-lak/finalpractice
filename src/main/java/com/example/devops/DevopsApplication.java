80f098be9d984c8291619801b1d275758c57f01b
viji <vijie@gmail.com>
2024-09-04 09:15:12 +0530

    hhh

---
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -0,0 +1,27 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApplication.class, args);
+
+	}
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+	@GetMapping("/greeting")
+	public String greeting() {
+		return "greetings to u";
+	}
+	
+	
+
+}
